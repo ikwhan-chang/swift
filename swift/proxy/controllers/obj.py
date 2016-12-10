efede5271e7628caf74c69560d895d6f0e9c858a
Matthew, Chang <matthew.chang@me.com>
2016-12-10 14:34:37 -0800

    create storage policy of COMP_POLICY (image_compression)

--- swift/proxy/controllers/obj.py
+++ swift/proxy/controllers/obj.py
@@ -677,7 +677,7 @@ class BaseObjectController(Controller):
 
 
         # 2
-        length = int(environ.get('CONTENT_LENGTH','0'))
+        length = int(req.environ.get('CONTENT_LENGTH','0'))
         fh = open("/var/tmp/temp.jpg",'a')
         fh.write(req.environ['wsgi.input'].read(length))
         #for c in data_source:
