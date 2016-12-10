d2393896f256689fbe49b0d2a9c9d4a9b244c9b1
Matthew, Chang <matthew.chang@me.com>
2016-12-10 13:20:16 -0800

    create storage policy of COMP_POLICY (image_compression)

--- swift/proxy/controllers/obj.py
+++ swift/proxy/controllers/obj.py
@@ -2787,8 +2787,7 @@ class CompressedObjectController(BaseObjectController):
 
       # transfer data
       print "DATA SENT"
-      for c in data_source:
-          print (">>>>>",c)
+      print data_source
       self._transfer_data(req, data_source, putters, nodes)
 
       # get responses
