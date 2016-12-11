233e9d9580aed7d7284a6034baebac6f3bb56c70
Matthew, Chang <matthew.chang@me.com>
2016-12-10 18:06:23 -0800

    create storage policy of COMP_POLICY (image_compression)

--- swift/obj/diskfile.py
+++ swift/obj/diskfile.py
@@ -1554,9 +1554,17 @@ class BaseDiskFileWriter(object):
           size = os.stat(img_file).st_size
           print "Compressed image file [{filename}] size = {size} bytes".format(filename=img_file, size=str(size))
 
-          command = "pwd;cp /var/tmp/"+data_file+" ./"+data_file+";swift -A http://10.240.0.5/auth/v1.0 -U test:tester -K testing upload Compressed "+image_name+"_compressed.jpg"
+          command = "pwd"
           print command
           subprocess.call([command])
+
+          command = "cp /var/tmp/"+data_file+" ./"+data_file
+        print command
+        subprocess.call([command])
+
+        command = "swift -A http://10.240.0.5/auth/v1.0 -U test:tester -K testing upload Compressed "+image_name+"_compressed.jpg"
+        print command
+        subprocess.call([command])
 
           ##
 
